82b42c6ec69d07fbab218185bae812c3241b33d2
Patryk Boczar <w71178@student.wsiz.edu.pl>
2024-12-19 14:32:35 +0100

    lab7

---
+++ lab7/Controllers/BookController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using lab6;
+
+namespace lab7.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class BookController : ControllerBase
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public BookController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        [HttpGet]
+        public IEnumerable<Book> GetAllBooks()
+        {
+            return _bookRepository.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Book> GetBookById(int id)
+        {
+            var book = _bookRepository.GetById(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return book;
+        }
+
+        [HttpPost]
+        public ActionResult<Book> CreateBook(Book book)
+        {
+            _bookRepository.Add(book);
+            return CreatedAtAction(nameof(GetBookById), new { id = book.Id }, book);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(int id, Book book)
+        {
+            if (id != book.Id)
+            {
+                return BadRequest();
+            }
+
+            var existingBook = _bookRepository.GetById(id);
+            if (existingBook == null)
+            {
+                return NotFound();
+            }
+
+            _bookRepository.Update(book);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            var book = _bookRepository.GetById(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _bookRepository.Remove(id);
+            return NoContent();
+        }
+    }
+}
---
+++ lab7/Controllers/PersonController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using lab6;
+
+namespace lab7.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class PersonController : ControllerBase
+    {
+        private readonly IPersonRepository _personRepository;
+
+        public PersonController(IPersonRepository personRepository)
+        {
+            _personRepository = personRepository;
+        }
+
+        [HttpGet]
+        public IEnumerable<Person> GetAllPeople()
+        {
+            return _personRepository.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Person> GetPersonById(int id)
+        {
+            var person = _personRepository.GetById(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            return person;
+        }
+
+        [HttpPost]
+        public ActionResult<Person> CreatePerson(Person person)
+        {
+            _personRepository.Add(person);
+            return CreatedAtAction(nameof(GetPersonById), new { id = person.Id }, person);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdatePerson(int id, Person person)
+        {
+            if (id != person.Id)
+            {
+                return BadRequest();
+            }
+
+            var existingPerson = _personRepository.GetById(id);
+            if (existingPerson == null)
+            {
+                return NotFound();
+            }
+
+            _personRepository.Update(person);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletePerson(int id)
+        {
+            var person = _personRepository.GetById(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            _personRepository.Remove(id);
+            return NoContent();
+        }
+    }
+}
